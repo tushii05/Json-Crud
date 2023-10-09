674efb20b0fda25e41081c764be6cb855311201d
tushii05 <tushardeshmukh0511@gmail.com>
2023-10-09 13:14:51 +0530

    Update

--- index.js
+++ index.js
@@ -6,33 +6,36 @@ const dataFile = 'data.json';
 
 app.use(express.json());
 
+let currentId = loadData().currentId || 1;
+
 function loadData() {
     try {
         const data = fs.readFileSync(dataFile, 'utf8');
         return JSON.parse(data);
     } catch (error) {
-        return [];
+        return { items: [], currentId: 1 };
     }
 }
+
 
 function saveData(data) {
     fs.writeFileSync(dataFile, JSON.stringify(data, null, 2), 'utf8');
 }
 
-let currentId = 1;
 
 app.post('/items', (req, res) => {
     const newItem = { id: currentId++, ...req.body };
     const data = loadData();
-    data.push(newItem);
+    data.items.push(newItem);
+    data.currentId = currentId;
     saveData(data);
-    res.status(201).json(newItem);
+    res.status(201).json({ message: 'Created Successfully', newItem });
 });
 
 app.get('/items/:id', (req, res) => {
     const id = parseInt(req.params.id);
     const data = loadData();
-    const item = data.find(item => item.id === id);
+    const item = data.items.find(item => item.id === id);
     if (item) {
         res.json(item);
     } else {
@@ -42,17 +45,18 @@ app.get('/items/:id', (req, res) => {
 
 app.get('/', (req, res) => {
     const data = loadData();
-    res.status(200).json(data);
+    res.status(200).json({ message: 'Successfully', data });
 });
+
 
 app.put('/items/:id', (req, res) => {
     const id = parseInt(req.params.id);
     const updatedItem = { id, ...req.body };
     const data = loadData();
-    const index = data.findIndex(item => item.id === id);
+    const index = data.items.findIndex(item => item.id === id);
 
     if (index !== -1) {
-        data[index] = updatedItem;
+        data.items[index] = updatedItem;
         saveData(data);
         res.json(updatedItem);
     } else {
@@ -60,19 +64,21 @@ app.put('/items/:id', (req, res) => {
     }
 });
 
+
 app.delete('/items/:id', (req, res) => {
     const id = parseInt(req.params.id);
     const data = loadData();
-    const index = data.findIndex(item => item.id === id);
+    const index = data.items.findIndex(item => item.id === id);
 
     if (index !== -1) {
-        data.splice(index, 1);
+        data.items.splice(index, 1);
         saveData(data);
         res.status(200).json({ message: 'Item Deleted' });
     } else {
         res.status(404).json({ message: 'Item not found' });
     }
 });
+
 
 
 app.listen(port, () => {
